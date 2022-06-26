30e9e75949b84658d609006aab97bd06846debf7
Julian-Bio0404 <juliancamilohermida@hotmail.com>
2022-06-26 13:59:15 -0500

    Asynch server

--- config/asgi.py
+++ config/asgi.py
@@ -8,9 +8,20 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 """
 
 import os
-
+import django
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from chat.routing import websocket_urlpatterns
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.local')
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.local')
 application = get_asgi_application()
+
+django.setup()
+
+application = ProtocolTypeRouter(
+    {
+        'websocket': AuthMiddlewareStack(URLRouter(websocket_urlpatterns)),
+    }
+)
