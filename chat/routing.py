2001906b58ff5def630c5da5fc693904a69ae458
Julian-Bio0404 <juliancamilohermida@hotmail.com>
2022-06-26 13:40:51 -0500

    Chat routing

---
+++ chat/routing.py
@@ -0,0 +1,9 @@
+"""Chat Urls."""
+
+from django.urls import re_path
+from chat.consumers import ChatConsumer
+
+
+websocket_urlpatterns = [
+    re_path(r'ws/chat/(?P<room_name>\w+)/$', ChatConsumer),
+]
