6a4ba7d10c07cc70c6def234c27c678d56916eab
Julian-Bio0404 <juliancamilohermida@hotmail.com>
2022-07-04 11:30:32 -0500

    Add urls

--- config/urls.py
+++ config/urls.py
@@ -2,7 +2,11 @@
 
 from django.contrib import admin
 from django.urls import path
+from chat.front.views import chat
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', chat),
+
 ]
